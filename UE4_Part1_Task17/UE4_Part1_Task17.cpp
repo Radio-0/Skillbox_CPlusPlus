ceb6ea30d54c9bfaa653594b6778963f7abdc3d2
Radio-0 <ant.bfmv@gmail.com>
2023-05-17 19:25:18 +0300

    Add Add UE4_P1_17...18

---
+++ UE4_Part1_Task17/UE4_Part1_Task17.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Vector {
+    int x, y, z;
+public:
+    Vector(int inX, int inY, int inZ) {
+        x = inX;
+        y = inY;
+        z = inZ;
+    }
+    void getX() {
+        cout << "X=" << x << endl;
+    }
+    void getY() {
+        cout << "Y=" << y << endl;
+    }
+    void getZ() {
+        cout << "Z=" << z << endl;
+    }
+    int lenghtVector() {
+        int lenght = 0;
+        lenght = sqrt(x * x + y * y + z * z);
+        return lenght;
+    }
+};
+
+int main() {
+    Vector vector(1, 2, 4);
+    vector.getX();
+    vector.getY();
+    vector.getZ();
+    cout << "Vector length: " << vector.lenghtVector();
+    return 0;
+}
---
+++ UE4_Part1_Task18/UE4_Part1_Task18.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+    string name = "";
+    int points = 0;
+public:
+    void setName(string inName) {
+        name = inName;
+    }
+    void setPoint(int inPoint) {
+        points = inPoint;
+    }
+    string getName() {
+        return name;
+    }
+    int getPoint() {
+        return points;
+    }
+};
+
+int main()
+{    
+    int sizeArr = 0;
+    string nameGamer;
+    int pointGamer;
+    cout << "How many players to add?\n";
+    cin >> sizeArr;
+    Person* person = new Person[sizeArr];
+    cout << "Enter name and points gamers: \n";
+    for (int i = 0; i < sizeArr; i++) {
+        cout << i + 1 << ":";        
+        cin >> nameGamer >> pointGamer;
+        person[i].setName(nameGamer);
+        person[i].setPoint(pointGamer);
+    }
+    for (int i = 0; i < sizeArr - 1; i++) {
+        for (int j = 0; j < sizeArr - 1; j++) {
+            Person tmp;
+            string nameTmp;
+            if (person[j].getPoint() > person[j + 1].getPoint()) {
+                tmp = person[j];
+                person[j] = person[j + 1];
+                person[j + 1] = tmp;
+            }
+        }
+    }
+    cout << "Show table Gamers:\n";
+    for (int i = 0; i < sizeArr; i++) {
+        cout << person[i].getName() << " " << person[i].getPoint() << endl;
+    }
+    delete[]person;
+    return 0;
+}
